08c627ece209c6f0060c9fd1bdfaeef8da58a7dc
Benjamin Logsdon <aeberhart@gmail.com>
2015-10-05 13:32:12 -0700

    updated function

--- getMSSMdata.R
+++ getMSSMdata.R
@@ -21,3 +21,38 @@ covariates <- fread(covariatesObj@filePath,data.table=F)
 covFP <- filter(covariates,TISSUE=='BM_10')
 covSTG <- filter(covariates,TISSUE=='BM_22')
 covPHG <- filter(covariates,TISSUE=='BM_36')
+
+###merge data
+
+expr2 <- t(expr)
+colnames(expr2) <- expr2[1,]
+expr2 <- cbind(rownames(expr2),expr2)
+colnames(expr2)[1] <- 'sampleId'
+gc()
+
+fpData <- merge(covFP,expr2,by.x = "Sample.ID", by.y = "sampleId" )
+row.names(fpData) <- fpData$Sample.ID
+stgData <- merge(covSTG,expr2,by.x = "Sample.ID", by.y = "sampleId")
+row.names(stgData) <- stgData$Sample.ID
+phgData <- merge(covPHG,expr2,by.x = "Sample.ID", by.y = "sampleId")
+row.names(phgData) <- phgData$Sample.ID
+
+###write files with cdr cutpoint
+fpDataControl <- filter(fpData,CDR %in% c('0','0.5','1'))
+fpDataCase <- filter(fpData,CDR %in% c('2','3','4','5'))
+
+stgDataControl <- filter(stgData,CDR %in% c('0','0.5','1'))
+stgDataCase <- filter(stgData,CDR %in% c('2','3','4','5'))
+
+phgDataControl <- filter(phgData,CDR %in% c('0','0.5','1'))
+phgDataCase <- filter(phgData,CDR %in% c('2','3','4','5'))
+
+
+write.csv(fpDataCase %>% select(starts_with("ENSG")),file='fpCase.csv',quote = F)
+write.csv(fpDataControl %>% select(starts_with("ENSG")),file='fpControl.csv',quote = F)
+write.csv(stgDataCase %>% select(starts_with("ENSG")),file='stgCase.csv',quote = F)
+write.csv(stgDataControl %>% select(starts_with("ENSG")),file='stgControl.csv',quote = F)
+write.csv(phgDataCase %>% select(starts_with("ENSG")),file='phgCase.csv',quote = F)
+write.csv(phgDataControl %>% select(starts_with("ENSG")),file='phgControl.csv',quote = F)
+
+
